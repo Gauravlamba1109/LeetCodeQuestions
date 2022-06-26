3add13c27c05fd7324e6bcf2d3d50c80db10f871
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-26 19:01:04 +0530

    Time: 172 ms (5.07%), Space: 39.8 MB (15.61%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -6,15 +6,18 @@ public:
         //bfs
         queue<int>q;
         q.push(start);
+        
         if(arr[start]==0) return true;
+        
         map<int,bool>m1;
         map<int,bool>m2;
+        
         m1[start]=1;
         m2[start]=1;
         
         while(q.size()>0){
-            int siz = q.size();
-            for(int i=0;i<siz;i++){
+            // int siz = q.size();
+            // for(int i=0;i<siz;i++){
                 int f = q.front(); q.pop();
                 int ind = f;
                 
@@ -23,25 +26,21 @@ public:
                 
                 if(newind1<arr.size()){
                     if(arr[newind1]==0) return true;
-                    
                     if(m1[newind1]==1) continue;
                     
                     q.push(newind1);
                     m1[newind1]=1;
-                    
                 }
+                
                 if(newind2>=0){
                     if(arr[newind2]==0) return true;
-                    
                     if(m2[newind2]==1) continue;
                     
                     q.push(newind2);
                     m2[newind2]=1;
-                    
-
                 }
-                
-            }
+            // }
+            
         }
         
         return false;
