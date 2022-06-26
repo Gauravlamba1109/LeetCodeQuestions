de2ed0465383a21cc233d6753d3d38840dc0efa6
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-26 19:10:20 +0530

    Time: 98 ms (17.74%), Space: 31.5 MB (71.44%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -2,47 +2,35 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         if(count(arr.begin(),arr.end(),0)==0) return false;
-        
         //bfs
         queue<int>q;
         q.push(start);
         
         if(arr[start]==0) return true;
         
-        map<int,bool>m1;
-        map<int,bool>m2;
-        
-        m1[start]=1;
-        m2[start]=1;
-        
         while(q.size()>0){
-            // int siz = q.size();
-            // for(int i=0;i<siz;i++){
-                int f = q.front(); q.pop();
-                int ind = f;
+            int siz = q.size();
+            for(int i=0;i<siz;i++){
+                int ind = q.front(); q.pop();
+                
+                if(arr[ind]<0) continue;
                 
                 int newind1 = ind+ arr[ind];
                 int newind2 = ind- arr[ind];
+
+                arr[ind] = -arr[ind];
                 
                 if(newind1<arr.size()){
                     if(arr[newind1]==0) return true;
-                    if(m1[newind1]==1) continue;
-                    
                     q.push(newind1);
-                    m1[newind1]=1;
                 }
                 
                 if(newind2>=0){
                     if(arr[newind2]==0) return true;
-                    if(m2[newind2]==1) continue;
-                    
                     q.push(newind2);
-                    m2[newind2]=1;
                 }
-            // }
-            
+            }
         }
-        
         return false;
     }
 };
