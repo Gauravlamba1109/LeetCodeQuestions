6f517f97abea1bc71813c89c1544053cbf17d768
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-26 19:39:02 +0530

    Time: 27 ms (47.15%), Space: 16.7 MB (23.20%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,23 +3,24 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         //dfs or bfs ? 
         // dfs first 
-        
         helper(graph,0,{});
         return ans;
     }
     
     vector<vector<int>>ans;
+    
     void helper(vector<vector<int>>& m,int curr,vector<int>path){
-        
         path.push_back(curr);
         
-        if(curr==m.size()-1){
+        if(curr==(m.size()-1)){
             ans.push_back(path);
+            return ;
         }else{
-            for(auto nei : m[curr])
+            for(auto nei : m[curr]){
                 helper(m,nei,path);    
+            }
         }
-        
         path.pop_back();
+        
     }
 };
