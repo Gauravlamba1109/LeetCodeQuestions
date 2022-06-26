c7db2e7fb0142a4b2f053631b57f888215c494a4
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-26 17:38:10 +0530

    Time: 110 ms (24.54%), Space: 42.5 MB (48.14%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+//         vector<vector<int>>m(cardPoints.size(),vector<int>(cardPoints.size(),0));
+        
+//         return helper(cardPoints,k,0,cardPoints.size()-1,m);
+    
+//         total - subarry = ans 
+//             maximize(total-subarray) 
+//             minimize subarray 
+            
+        int i=0; int j=cardPoints.size()-k;
+        int total = accumulate(cardPoints.begin(),cardPoints.end(),0);
+        int sum = 0;
+        for(int i=0;i<j;i++)sum+=cardPoints[i];
+        // cout<<sum<<" "<<k<<" ";
+        int ans = sum;
+        while(i<j && j<cardPoints.size()){
+            // cout<<i<<","<<j<<" "<<cardPoints[i]<<" "<<cardPoints[j]<<" "<<sum<<endl;
+            sum = sum+cardPoints[j];
+            sum = sum-cardPoints[i];
+            
+            ans = min(ans,sum);
+            i++;j++;
+        }
+        return total-ans;
+    }
+    
+    int helper(vector<int>&cards,int k,int i,int j,vector<vector<int>>&m){
+        if(i>j || cards.size()==0 || k==0) return 0;
+        if(k<0) return 0;
+        if(m[i][j]>0) return m[i][j];
+        int fs = cards[i] + helper(cards,k-1,i+1,j,m);
+        int fe = cards[j] + helper(cards,k-1,i,j-1,m);
+        
+        return m[i][j]=max(fs,fe);
+    }
+};
