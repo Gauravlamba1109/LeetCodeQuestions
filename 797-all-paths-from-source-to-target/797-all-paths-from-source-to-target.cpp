0e85716093988e998a8517a11f08ce5b391d75a4
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-26 19:35:39 +0530

    Time: 77 ms (10.52%), Space: 16.5 MB (26.62%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -4,32 +4,22 @@ public:
         //dfs or bfs ? 
         // dfs first 
         
-        int n =graph.size()-1;
-        vector<bool>visited(n+1,0);
-        
-        helper(graph,visited,n,0,{});
+        helper(graph,0,{});
         return ans;
     }
     
     vector<vector<int>>ans;
-    void helper(vector<vector<int>>& m, vector<bool>&visited,int dest,int curr,vector<int>path){
-        if(curr>dest) return ;
-        if(curr==dest){
-            path.push_back(curr);
-            ans.push_back(path);
-            path.pop_back();
-            return ;
-        }
+    void helper(vector<vector<int>>& m,int curr,vector<int>path){
         
-        visited[curr]=1;
         path.push_back(curr);
         
-        for(auto nei : m[curr])
-            if(!visited[nei]) 
-                helper(m,visited,dest,nei,path);    
+        if(curr==m.size()-1){
+            ans.push_back(path);
+        }else{
+            for(auto nei : m[curr])
+                helper(m,nei,path);    
+        }
         
-        
-        visited[curr]=0;
         path.pop_back();
     }
 };
