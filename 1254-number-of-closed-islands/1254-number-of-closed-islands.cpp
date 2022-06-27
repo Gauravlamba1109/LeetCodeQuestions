5d60627714e723818fb9e847ad28f941ab9e440b
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-27 06:12:43 +0530

    Time: 19 ms (51.87%), Space: 9.5 MB (35.18%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -5,35 +5,26 @@ public:
         // a 0 on edge make it 1 
         // after that we are left with only islands inside the grid
         
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(i==0 || j==0 || i==(grid.size()-1) || j==(grid[0].size()-1) ){
-                    if(grid[i][j]==0) 
-                        dfs(grid,i,j);
-                }
-            }
-        }
         int cnt=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==0){
-                    dfs(grid,i,j);
-                    cnt++;
-                }
+                if(grid[i][j]==0) 
+                    cnt+=dfs(grid,i,j);
             }
         }
         return cnt;
     }
     
-    void dfs(vector<vector<int>>&grid,int i ,int j){
-        if(i<0 || j<0 || i>grid.size()-1 || j>grid[0].size()-1 || grid[i][j]==1) return ;
+    bool dfs(vector<vector<int>>&grid,int i ,int j){
+        if(i==0 || j==0 || i==(grid.size()-1) || j==(grid[0].size()-1) ) return 0;
         
         grid[i][j]=1;
+        bool ans=1;
+        if((i+1)<grid.size() && grid[i+1][j]==0) ans&=dfs(grid,i+1,j);
+        if((j+1)<grid[0].size() && grid[i][j+1]==0) ans&=dfs(grid,i,j+1);
+        if((i-1)>=0 && grid[i-1][j]==0) ans&=dfs(grid,i-1,j);
+        if((j-1)>=0 && grid[i][j-1]==0) ans&=dfs(grid,i,j-1);
         
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
-
+        return ans;
     }
 };
