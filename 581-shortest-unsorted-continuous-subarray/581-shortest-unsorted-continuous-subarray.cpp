2b49f21faaab73cb4b47cf338c5ac6e2f721efbd
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-29 00:01:31 +0530

    Time: 74 ms (8.32%), Space: 26.5 MB (66.27%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+        
         if(nums.size()==1) return 0;
+        int ma=nums[0];
+        int mi = nums[nums.size()-1];
+        int start = -1;
+        int end = -2;
+        
         //longest unsorted subarray 
-        priority_queue<int>q;
-        int mi=0;
-        for(int i=0;i<nums.size();i++){
-            if(q.size()>0 && q.top()>nums[i]){
-                mi = max(mi,i);
+        for(int i=1;i<nums.size();i++){
+            int j=nums.size()-1-i;
+            
+            ma = max(ma,nums[i]);
+            if(ma>nums[i]){
+                end = i;
+            } 
+            mi = min(mi,nums[j]);
+            if(mi<nums[j]){
+                start = j;
             }
-            q.push(nums[i]);
         }
-        
-        int ma = nums.size()-1;
-        priority_queue<int, vector<int>, greater<int> > p;
-        for(int i=nums.size()-1;i>=0 ; i--){
-            if(p.size()>0 && p.top()<nums[i]){
-                ma = min(ma,i);
-            }
-            p.push(nums[i]);
-        }
-        // cout<<mi<<" "<<ma<<endl;
-        if(ma>mi) return 0;
-        int ans = mi-ma + 1;
-        
-        return ans;
+
+        return end-start+1;
     }
 };
 
