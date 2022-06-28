2c4639055a35c0d49dc18b70e1014a6255031039
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-29 00:53:24 +0530

    Time: 18 ms (69.70%), Space: 10.6 MB (52.41%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         // we will keep a pq to have the trip end so that we can se 
-        // if the curr node ; if the starting is more than the ending of a prev 
+        // if the starting is more than the ending of a prev
+        // then we pop the trips which ended ; and add the curr trip to q
+        
         sort(trips.begin(),trips.end(), [](const vector<int>&a , const vector<int>&b ){
            return a[1]<b[1]; 
         });
-        // for(auto ele: trips)cout<<ele[0]<<","<<ele[1]<<","<<ele[2]<<endl;
-        // cout<<endl;
+
+        
+        // to get the latest trip which ended ; push trip-end and index
         priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> >q;
         
-        // q.push({trips[0][2],0}); 
+        // starting cap 
         int currcap =0;
         
         for(int i=0;i<trips.size();i++){
-           // cout<<currcap<<";"<<q.top().first<<","<<q.top().second<<endl;
             vector<int> currt = trips[i];
             currcap += currt[0];
+            
+            // del ended trips 
             while(q.size() && q.top().first<= currt[1] ){
-                // this trip will move simult
                 currcap -= trips[q.top().second][0];
                 q.pop();
             }
+            // exceeded cap 
             if(currcap>capacity) return false;
+            // add curr trip 
             q.push({currt[2],i});
         }
         
