2412510de29b2164eb8345af6724bab2f3d37f09
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-29 07:42:50 +0530

    Time: 344 ms (12.72%), Space: 13 MB (29.18%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        
+        // sort from tall to short 
+        // and place ele using k value 
         sort(people.begin(),people.end(),[](const vector<int>&a,const vector<int>&b){
-            return a[0]<b[0];
+            return( a[0]>b[0]  || (a[0]==b[0] && a[1]<b[1])) ;
         });
-        vector<vector<int>>ans(people.size(),vector<int>(2,-1));
+        for(auto ele : people)cout<<ele[0]<<","<<ele[1]<<" ";
         
+        vector<vector<int>>ans;
         for(auto ele : people){
-            int k = ele[1];
-            for(int i=0;i<ans.size();i++){
-                if(ans[i][0]==-1 && k==0){
-                    ans[i][0]=ele[0];
-                    ans[i][1]=ele[1];
-                    break;
-                }
-                else if(ans[i][0]==-1 || ans[i][0]>=ele[0]) 
-                    k--;
-            }
+            auto it = ans.begin();
+            ans.insert(it+ele[1],ele);
         }
+        
         return ans;
+        
+        
+        // sort by height 
+        // then place each on its appropiate position 
+        // use the k value ; if the curr place is not taken or value is greater dec k ; 
+        // ele if k==0 place it 
+//         sort(people.begin(),people.end(),[](const vector<int>&a,const vector<int>&b){
+//             return a[0]<b[0];
+//         });
+//         vector<vector<int>>ans(people.size(),vector<int>(2,-1));
+        
+//         for(auto ele : people){
+//             int k = ele[1];
+//             for(int i=0;i<ans.size();i++){
+//                 if(ans[i][0]==-1 && k==0){
+//                     ans[i][0]=ele[0];
+//                     ans[i][1]=ele[1];
+//                     break;
+//                 }
+//                 else if(ans[i][0]==-1 || ans[i][0]>=ele[0]) 
+//                     k--;
+//             }
+//         }
+//         return ans;
     }
 };
