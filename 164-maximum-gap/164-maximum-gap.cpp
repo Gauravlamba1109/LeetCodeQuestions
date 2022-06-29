44d4a1a699a2cb4d09a5d5865d6d575e0d1f0993
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-29 23:39:26 +0530

    Time: 240 ms (66.27%), Space: 72.3 MB (77.93%) - LeetHub

---
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()<2) return 0 ;
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=1;i<nums.size();i++){
+            ans=max(ans,nums[i]-nums[i-1]);
+        }
+        return ans;
+    }
+};
