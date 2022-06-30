3337a9205d539e8a456d8ea815cfb3b535e80d1f
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-06-30 05:59:33 +0530

    Time: 30 ms (35.28%), Space: 17.9 MB (8.81%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        stack<vector<int>>st;
+        bool taken = false;
+        if(intervals.size()==0) return {newInterval};
+        
+        for(auto ele:intervals){
+            if(ele[0]>=newInterval[0] && !taken){
+                taken= true ; 
+                
+                //push interval into stack 
+                if(st.size() && st.top()[1]>=newInterval[0])
+                    st.top() = {st.top()[0],max(newInterval[1],st.top()[1])};
+                else{
+                    st.push(newInterval);
+                }
+                // cout<<st.top()[0]<<','<<st.top()[1]<<endl;
+                
+                // push ele 
+                if(st.top()[1]>=ele[0])
+                    st.top() = {st.top()[0],max(ele[1],st.top()[1])};
+                else 
+                    st.push(ele);
+                
+                
+            }else{
+                if(st.size() && st.top()[1]>=ele[0])
+                    st.top() = {st.top()[0],max(ele[1],st.top()[1])};
+                else 
+                    st.push(ele);
+            }
+        }
+        if(!taken){
+            if(st.size() && st.top()[1]>=newInterval[0])
+                st.top() = {st.top()[0],max(newInterval[1],st.top()[1])};
+            else st.push(newInterval);
+        }
+
+        vector<vector<int>>ans;
+        while(st.size()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
