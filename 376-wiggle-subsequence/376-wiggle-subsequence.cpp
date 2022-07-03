c687abec50233b6a041c6ac60b3782af049cd25f
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-03 19:01:02 +0530

    Time: 4 ms (56.21%), Space: 7 MB (95.65%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        
-        vector<int>dif;
-        for(int i=0;i<nums.size()-1;i++){
-            dif.push_back(nums[i+1]-nums[i]);
-        }
-        
         bool p = true; int cntp=0;
         bool n = true; int cntn=0;
-        for(int i=0;i<dif.size();i++){
+        
+        for(int i=0;i<nums.size()-1;i++){
             // cout<<dif[i]<<" -> ";
-            if((p & dif[i]>0)|| (!p & dif[i]<0)){ 
+            if( (p & nums[i+1]>nums[i]) || (!p & nums[i+1]<nums[i]) ){ 
                 cntp++;
                 p=!p;
             }
             
-            if((n & dif[i]<0) || (!n & dif[i]>0)){ 
+            if( (n & nums[i+1]<nums[i]) || (!n & nums[i+1]>nums[i]) ){ 
                 cntn++;
                 n=!n;
             }
