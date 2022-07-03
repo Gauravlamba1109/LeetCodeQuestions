a4ec734daea0d9299660b9f69e17b517b530e293
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-03 18:57:25 +0530

    Time: 10 ms (30.89%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -7,35 +7,20 @@ public:
             dif.push_back(nums[i+1]-nums[i]);
         }
         
-        int startingpos = 0; bool p = true; int cntp=0;
-        int startingneg = -1;bool n = true; int cntn=0;
+        bool p = true; int cntp=0;
+        bool n = true; int cntn=0;
         for(int i=0;i<dif.size();i++){
             // cout<<dif[i]<<" -> ";
-            if(p){
-                if(dif[i]>0){ 
-                    cntp++;
-                    p=false;
-                }
-            }
-            else if(!p){
-                if(dif[i]<0){ 
-                    cntp++;
-                    p=true;
-                }
+            if((p & dif[i]>0)|| (!p & dif[i]<0)){ 
+                cntp++;
+                p=!p;
             }
             
-            if(n){
-                if(dif[i]<0){ 
-                    cntn++;
-                    n=false;
-                }
+            if((n & dif[i]<0) || (!n & dif[i]>0)){ 
+                cntn++;
+                n=!n;
             }
-            else if(!n){
-                if(dif[i]>0){ 
-                    cntn++;
-                    n=true;
-                }
-            }
+            
             // cout<<cntp<<" "<<cntn<<endl;
         }
         return max(cntn,cntp)+1;
