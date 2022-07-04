02e720fe45b3f662ef86f70989de4fc3b47c0f78
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-04 06:53:50 +0530

    Time: 9 ms (60.00%), Space: 7 MB (40.00%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -5,23 +5,16 @@ public:
         unordered_map<char,char>m;
         int k=0;
         for(int i=0;i<key.size();i++){
-            if(key[i]==' ')continue;
             char c = key[i];
-            if(m.find(c)==m.end()){
-                m[c]= k+'a';
-                k++;
-            }
+            if(c ==' ')continue;
+            if(m.find(c)==m.end()) m[c]= 'a'+ k++;
             if(m.size()>25) break;
-            cout<<c<<"-"<<m[c]<<" ";
         }
         
         for(int i=0;i<message.size();i++){
             char c = message[i];
             if(c==' ')ans+=' ';
-            else{
-                ans+= (char)m[c];
-                cout<<c<<" "<<m[c]<<endl;
-            }
+            else ans+= (char)m[c];
         }
         return ans;
     }
