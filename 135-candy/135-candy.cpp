e0bc81eb5107ee1f7f25e30c3885c799509ff89c
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-04 23:48:32 +0530

    Time: 96 ms (7.74%), Space: 20.3 MB (6.87%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -5,21 +5,18 @@ public:
         vector<int>cand(ratings.size(),1);
         priority_queue<pair<int,int>>q;
         
-        for(int i=0;i<ratings.size();i++){
-            q.push({-ratings[i],i});
-        }
+        for(int i=0;i<ratings.size();i++) q.push({-ratings[i],i});
+        
+        int n=ratings.size();
         
         while(q.size()){
-            pair<int,int>t = q.top(); q.pop();
-            int rat = t.first;
-            int ind = t.second;
+            int ind = q.top().second; q.pop();
             
-            if(ind-1>=0 && ratings[ind]>ratings[ind-1] && ind+1<ratings.size() && ratings[ind]>ratings[ind+1] )
+            if(ind-1>=0 && ratings[ind]>ratings[ind-1] && ind+1<n && ratings[ind]>ratings[ind+1] )
                 cand[ind]=max(cand[ind-1],cand[ind+1])+1;
-            else if(ind-1>=0 && ratings[ind]>ratings[ind-1] )
-                cand[ind]=cand[ind-1]+1;
-            else if(ind+1<ratings.size() && ratings[ind]>ratings[ind+1] )
-                cand[ind]=cand[ind+1]+1;
+            
+            else if(ind-1>=0 && ratings[ind]>ratings[ind-1] ) cand[ind]=cand[ind-1]+1;
+            else if(ind+1<n &&  ratings[ind]>ratings[ind+1] ) cand[ind]=cand[ind+1]+1;
         }
         
         return accumulate(cand.begin(),cand.end(),0);
