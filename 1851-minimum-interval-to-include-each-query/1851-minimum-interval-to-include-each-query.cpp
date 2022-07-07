2476439f34730d9539cfe987df45111f528362e3
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-06 23:57:38 -0700

    Time: 1279 ms (21.08%), Space: 139.4 MB (27.25%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& a, vector<int>& queries) {
+        // * Sort queries and intervals.
+        // * Iterate queries from small to big,
+        // * and find out all open intervals [l, r],
+        // * and we add them to a priority queue.
+        // * Also, we need to remove all closed interval from the queue.
+        // * 
+        // * In the priority, we use
+        // * [interval size, interval end] = [r-l+1, r] as the key.
+        // * 
+        // * The head of the queue is the smallest interval we want to return for each query.
+        // *
+        // Complexity
+        // Time O(nlogn + qlogq)
+        // Space O(n+q)
+        // where q = queries.size()
+
         priority_queue<vector<int>>pq;
         unordered_map<int,int>res;
         vector<int>q= queries , res2;
@@ -12,11 +28,11 @@ public:
         
         for(int qq:q){
             while(i<n && a[i][0]<=qq){
-                int l = a[i][0], r = a[i++][1];
-                pq.push({l-r-1,r});
+                pq.push({(-a[i][1]+a[i][0]-1),a[i][1]});
+                i++;
             }
-            while(pq.size() && pq.top()[1]<qq) pq.pop();
-            res[qq]=pq.size()? -pq.top()[0]:-1 ;
+            while(!pq.empty() && pq.top()[1]<qq) pq.pop();
+            res[qq]=pq.size()?-pq.top()[0]:-1 ;
         }
         
         for(int qq:queries) res2.push_back(res[qq]);
