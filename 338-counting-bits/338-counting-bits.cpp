b2e63b4d82913ed95d746381786680ae6f05d983
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-07 10:04:49 -0700

    Time: 4 ms (88.45%), Space: 8.6 MB (21.24%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        
-        for(int i=0;i<=n;i++){
-            string nn = bitset<32>(i).to_string();
-            ans.push_back(count(nn.begin(),nn.end(),'1'));
+        // bit + dp solution 
+        vector<int>dp;
+        int offset = 1; int pow = 2;
+        dp.push_back(0);
+        for(int i=1;i<=n;i++){
+            dp.push_back(0);
+            int cnt=0;
+            if(offset*pow==i){
+                dp[i]=1;
+                offset = offset*pow;
+            }else{
+                dp[i]=dp[i-offset]+1;
+            }
         }
-        return ans;
+        return dp;
+        // string solution 
+//         vector<int>ans;
+        
+//         for(int i=0;i<=n;i++){
+//             string nn = bitset<32>(i).to_string();
+//             ans.push_back(count(nn.begin(),nn.end(),'1'));
+//         }
+//         return ans;
     }
 
 };
