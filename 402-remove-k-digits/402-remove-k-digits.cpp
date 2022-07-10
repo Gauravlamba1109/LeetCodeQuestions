1a08bbc5fd56dda2fa040d311bfcdb1c0fd4762f
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-10 06:57:19 +0530

    Update 402-remove-k-digits.cpp

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         // monotonic stack 
+        // basic idea is :
+        // we have to delete peaks which are occuring before curr element
+        // we can remove only k times 
+        
         int n=num.size();
         if(n<=k) return "0";
-        int i=-1;
-        for(char &c:num){
-            while(k>0 && i>-1 && num[i]>c){
-                i--;
-                k--;
-            }
-            // if(i>-1 && c!='0') 
-                num[++i]=c;
-        }
-        // cout<<num<<" "<<i<<endl;
-        while(i>-1 && k--)i--;
-        num=num.substr(0,i+1); // cut rem 
-        i=0;
+        // used num as a stack itself 
         
-        while(i<num.size() && num[i]=='0'){
-            i++;
+        int i=-1;
+        for(char & c : num){
+            while( k>0 && i>-1 && num[i] > c ){ 
+                                            // if curr is occuring after a peak ; del peak 
+                i--;  k--;
+            }
+            num[++i] = c;                   // add curr to stack now 
         }
-        num=num.substr(i); // cut 0s 
-        if(num=="") return "0";
-        return num;
+
+        while( i>-1 && k-- ) i--;           // if we can still remove ele , remove
+        num = num.substr(0,i+1);              // cut stack from rem 
+        
+        i=0;
+        while(i<num.size() && num[i] == '0' )
+            i++;                            // index of non-0 starting ; del leading zeros 
+        num = num.substr(i);                // cut 0s 
+        
+        return num=="" ? "0" : num;         // if all were 0s we are left with empty str 
     }
 };
