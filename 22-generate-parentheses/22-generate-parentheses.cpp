f48930a029ee25d05772a38d387d2bc75fbfb9ff
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-09 19:03:08 -0700

    Time: 39 ms (6.01%), Space: 29.9 MB (5.13%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<string>ans;
+    // rec 
     void helper(int n , string curr, int total,int open){
         if(curr.size()>total || open<0) return ; 
         if(curr.size()==total && open==0){
             ans.push_back(curr);
+            return ;
         }
         // open brac 
         if(open<=n) helper(n,curr+'(',total,open+1);
@@ -13,7 +15,10 @@ public:
     }
     
     vector<string> generateParenthesis(int n) {
+        // rec
         helper(n,"",n*2,0);
+        
+        
         return ans;
     }
 };
