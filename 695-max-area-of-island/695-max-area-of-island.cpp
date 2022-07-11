242e383753ab6f6d253aa605f2e36ee4bc8737f7
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-11 00:00:02 -0700

    Time: 105 ms (5.05%), Space: 30 MB (5.95%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -12,20 +12,17 @@ public:
         return ans;
     }
     vector<vector<int>>dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    
     int dfs(int i,int j,vector<vector<int>>&grid, int n,int m){
-        if(i>=n || j>=m || i<0 || j<0) return 0;
-        if(grid[i][j]==0) return 0;
+        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==0) return 0;
         
         grid[i][j]=0;
+        
         int ans = 1;
         
-        for(vector<int> pos: dir){
-            int ni= i+pos[0], nj=j+pos[1];
-            if(ni<n && ni>=0 && nj<m && nj>=0 &&  grid[ni][nj]==1){
-                ans+= dfs(ni,nj,grid,n,m);
-            }
-        }
-        
+        for(vector<int> pos: dir)
+            ans+= dfs(i+pos[0],j+pos[1],grid,n,m);
+            
         return ans;
     }
 };
