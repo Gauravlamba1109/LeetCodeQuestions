8eb6490c80a32fa77dc593b408edd07d23e3f17e
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-12 01:29:36 -0700

    Time: 389 ms (49.53%), Space: 10.1 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -6,24 +6,24 @@ public:
             
         int sum = accumulate(m.begin(),m.end(),0);
         int partsum = sum/4;
-        if(sum%4) return false;
-        
+        if(partsum*4!=sum) return false;
         sort(m.rbegin(),m.rend());
-        vector<int>sumi(4,0);
-        
-        return possible(m,partsum,sumi,0);
+        return possible(m,partsum,0,0,0,0,0);
     }
     
-    bool possible(vector<int>&m,int part, vector<int>&sum,int i){
-        if(i>=m.size())
-            return ((sum[0]==sum[1]) && (sum[1]==sum[2]) && (sum[2]==part));
+    
+    
+    bool possible(vector<int>&m,int sum,int a, int b,int c,int d,int i){
+        if(i==m.size()){
+            if(a==b && b==c && c==d) return true;
+            else return false;
+        }    
         
-        for(int j=0;j<4;j++){
-            if((m[i]+sum[j]) > part) continue;
-            sum[j]+=m[i];
-            if( possible(m,part,sum,i+1) ) return true;
-            sum[j]-=m[i];
-        }
+        if((a+m[i])<=sum && possible(m,sum,a+m[i],b,c,d,i+1)) return true;
+        if((b+m[i])<=sum && possible(m,sum,a,b+m[i],c,d,i+1)) return true;
+        if((c+m[i])<=sum && possible(m,sum,a,b,c+m[i],d,i+1)) return true;
+        if((d+m[i])<=sum && possible(m,sum,a,b,c,d+m[i],i+1)) return true;
+
         return false;
     }
 };
