0b9c381e4d58a71be2f295b8da35b61b4c7835de
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-17 21:37:19 -0700

    Time: 929 ms (75.04%), Space: 150.7 MB (70.85%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -5,23 +5,22 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         
+        // each row will have sum of columns of that row 
         for(int i=0;i<n;i++){
             for(int j=1;j<m;j++){
                 mat[i][j]+=mat[i][j-1];
             }
         }
-        
         int ans=0;
         for(int colstart=0; colstart<m; colstart++){
             for(int colend=colstart ; colend<m ; colend++){
-                for(int rowstart=0 ; rowstart<n; rowstart++){
-                    int sum=0;
-                    for(int rowend=rowstart ;rowend<n; rowend++){
-                        sum += (mat[rowend][colend]-(colstart?mat[rowend][colstart-1] : 0));
-                        if(sum==target){
-                            ans++;
-                        }
-                    }
+                unordered_map<int,int> mp;
+                mp[0]=1;
+                int sum=0;
+                for(int row=0 ; row<n; row++){
+                    sum += (mat[row][colend]-(colstart?mat[row][colstart-1] : 0));
+                    if(mp.find(sum-target)!=mp.end()) ans+=mp[sum-target];
+                    mp[sum]++;
                 }
             }
         }
