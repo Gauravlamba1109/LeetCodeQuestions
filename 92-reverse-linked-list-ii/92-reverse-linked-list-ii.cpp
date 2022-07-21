0ddc92793b475c72d9cfae7bc77c90efff814f00
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-20 23:37:52 -0700

    Time: 3 ms (72.05%), Space: 7.4 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,37 +10,75 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) {
-        // 
-        if(head==NULL) return NULL;
-        // move to left 
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        while(m>1){
-            prev=curr;
-            curr=curr->next;
-            m--;
-            n--;
-        }
-        ListNode* res = prev;
-        ListNode* tail= curr;
-        // curr is on left pos 
-        // we can start reversing till n is 0 
-        while(n>0){
-            ListNode* nex = curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr=nex;
-            n--;
-        }
+    ListNode* reverseAll(ListNode* head){
+        // reverse full list 
+        if(head->next==NULL || head==NULL) return head;
+        ListNode* last = reverseAll(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return last;
         
-        // r   t     p  c
-        // 1-> 2<-3<-4  5
-        if(res) res->next=prev;
-        else head = prev;
-        tail->next= curr;
+    
+    }
+    
+    // reverse N from start
+    ListNode* succ = NULL;
+    
+    ListNode* reverseN(ListNode* head, int n){
+        if(n==1){ 
+            succ=head->next; 
+            return head;
+        }
+        ListNode* last = reverseN(head->next,n-1);
+        head->next->next = head;
+        head->next=succ;
+        return last;
+    }
+        
+    
+    
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+
+        // now from m to n 
+        if(m==1) 
+            return reverseN(head,n);
+        head->next = reverseBetween(head->next,m-1,n-1);
         
         return head;
+
+        
+        
+        
+//         // 
+//         if(head==NULL) return NULL;
+//         // move to left 
+//         ListNode* prev=NULL;
+//         ListNode* curr=head;
+//         while(m>1){
+//             prev=curr;
+//             curr=curr->next;
+//             m--;
+//             n--;
+//         }
+//         ListNode* res = prev;
+//         ListNode* tail= curr;
+//         // curr is on left pos 
+//         // we can start reversing till n is 0 
+//         while(n>0){
+//             ListNode* nex = curr->next;
+//             curr->next = prev;
+//             prev=curr;
+//             curr=nex;
+//             n--;
+//         }
+        
+//         // r   t     p  c
+//         // 1-> 2<-3<-4  5
+//         if(res) res->next=prev;
+//         else head = prev;
+//         tail->next= curr;
+        
+//         return head;
         
         
         
