f59254529563f03eb847d32bb691ce30f6c468f2
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-24 11:03:20 -0700

    Time: 178 ms (66.67%), Space: 34.7 MB (33.33%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string,int>mr;
-        unordered_map<string,int>mc;
-
-        for(int i=0;i<grid.size();i++){
-            string row = "";
-            for(int j=0;j<grid[0].size();j++){
-                row+=( "#" + to_string(grid[i][j]));
-            }
-            mr[row]++;
+        map<vector<int>,int>m;
+        for(auto v : grid){
+            m[v]++;
         }
         
+        int ans = 0;
         for(int j=0;j<grid[0].size();j++){
-            string col = "";
+            vector<int> c;
             for(int i=0;i<grid.size();i++){
-                col+=( "#" + to_string(grid[i][j]));
+                c.push_back(grid[i][j]);
             }
-            mc[col]++;
-        }
-        
-        int ans = 0 ;
-        for(auto r : mr){
-            if(mc.find(r.first)!=mc.end()){
-                ans+=( r.second*mc[r.first] );
-            }
-        }
+            ans+=m[c];
+        }        
         return ans;
+        
+        // cashing 
+        // unordered_map<string,int>mr;
+        // unordered_map<string,int>mc;
+
+//         for(int i=0;i<grid.size();i++){
+//             string row = "";
+//             for(int j=0;j<grid[0].size();j++){
+//                 row+=( "#" + to_string(grid[i][j]));
+//             }
+//             mr[row]++;
+//         }
+        
+//         for(int j=0;j<grid[0].size();j++){
+//             string col = "";
+//             for(int i=0;i<grid.size();i++){
+//                 col+=( "#" + to_string(grid[i][j]));
+//             }
+//             mc[col]++;
+//         }
+        
+//         int ans = 0 ;
+//         for(auto r : mr){
+//             if(mc.find(r.first)!=mc.end()){
+//                 ans+=( r.second*mc[r.first] );
+//             }
+//         }
+//         return ans;
     }
 };
