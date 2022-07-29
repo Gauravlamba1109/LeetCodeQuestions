877e613377afebaa27e854b87052a296ea8c0790
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-07-29 11:15:08 -0700

    Time: 8 ms (44.61%), Space: 8.4 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -3,25 +3,43 @@ public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string>ans;
         
+        // for(auto w : words){
+        //     if(match(w,pattern))ans.push_back(w);
+        // }
+        
+        // or 
+        string p = f(pattern);
         for(auto w : words){
-            if(match(w,pattern))ans.push_back(w);
+            if(f(w)==p)ans.push_back(w);
         }
         return ans;
     }
     
-    bool match(string w,string pattern){
-        map<char,char>m1;
-        map<char,char>m2;
-        
-        for(int i=0;i<w.size();i++){
-            char a = w[i];
-            char b = pattern[i];
-            
-            if(m1.find(a)==m1.end()) m1[a]=b;
-            if(m2.find(b)==m2.end()) m2[b]=a;
-            
-            if(m1[a]!=b || m2[b]!=a) return false;
+    
+    string f(string w){
+        unordered_map<char,int>m;
+        for(auto c : w){
+            if(m.find(c)==m.end()) m[c]=m.size();
         }
-        return true;
+        for(auto &c : w){
+            c = 'a'+m[c];
+        }
+        return w ; 
     }
+    
+//     bool match(string w,string pattern){
+//         map<char,char>m1;
+//         map<char,char>m2;
+        
+//         for(int i=0;i<w.size();i++){
+//             char a = w[i];
+//             char b = pattern[i];
+            
+//             if(m1.find(a)==m1.end()) m1[a]=b;
+//             if(m2.find(b)==m2.end()) m2[b]=a;
+            
+//             if(m1[a]!=b || m2[b]!=a) return false;
+//         }
+//         return true;
+//     }
 };
