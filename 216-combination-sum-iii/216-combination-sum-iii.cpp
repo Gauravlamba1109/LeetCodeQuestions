ebfe87804c1787fe1e5fd1c5d0ab78f90dc83555
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-05 04:13:39 -0700

    Time: 6 ms (21.38%), Space: 6.4 MB (60.81%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -3,25 +3,23 @@ public:
     vector<vector<int>>ans;
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>sofar;
-        solve(k,n,sofar);
+        solve(k,n,sofar,1);
         return ans;
     }
     
-    void solve(int k,int n,vector<int>&sofar){
+    void solve(int k,int n,vector<int>&sofar,int start){
         if(n<0) return ;
-        if(sofar.size()==k && n!=0) return ;
-        if(n==0 ){
-            if(sofar.size()!=k) return ;
-            ans.push_back(sofar);
+        
+        if(sofar.size()==k){
+            if(n==0 ){
+                ans.push_back(sofar);
+            }
             return ;
         }
-        int start;
-        if(sofar.size()==0)start=1;
-        else start= sofar.back()+1;
         
         for(int s=start; s<=min(n,9); s++){
             sofar.push_back(s);
-            solve(k,n-s, sofar);
+            solve(k,n-s, sofar,s+1);
             sofar.pop_back();
         }
         
