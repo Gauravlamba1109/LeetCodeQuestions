9567481efdc008db3464010fdda9f4865862a687
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-10 12:03:51 -0700

    Time: 306 ms (63.65%), Space: 78.2 MB (79.91%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int sum=0;
+        unordered_map<int,int>m;
+        
+        for(auto ele : arr){
+            m[ele]++; 
+        }
+        
+        vector<int>a;
+        for(auto ele : m){
+            a.push_back(ele.second);
+            sum+=ele.second;
+        }
+        
+        sort(a.begin(),a.end(), greater<int>());
+        int des= sum - (sum/2), cnt=0 , i=0;
+        while(i<a.size() && des>0){
+            cnt++;
+            des-=a[i];
+            i++;
+        }
+        
+        return cnt;
+    }
+  
+};
