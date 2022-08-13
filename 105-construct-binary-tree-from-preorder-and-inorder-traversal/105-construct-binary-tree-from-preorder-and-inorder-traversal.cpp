45a86505306c5ecb0fa5c9b0be9a40b25e86d97c
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-13 09:42:24 -0700

    Time: 39 ms (50.50%), Space: 26 MB (77.11%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,90 +11,21 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int ps , int pe , int is ,int ie){
-        if(is>ie) return NULL;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
+    }
+    
+    TreeNode* helper(vector<int>& preorder,int ps, int pe, vector<int>& inorder, int is,int ie){
+        if(ps>pe || is>ie) return NULL;
+        
         TreeNode* root = new TreeNode(preorder[ps]);
         
-        // abs position 
-        int it = find(inorder.begin()+is,inorder.begin()+ie,preorder[ps])-(inorder.begin());
+        int it = find(inorder.begin(),inorder.end(),preorder[ps])-inorder.begin();
+        int noleft = it-is;
         
-        root->left = helper(preorder,inorder,ps+1,ps+(it-is),is,it-1);
-        root->right= helper(preorder,inorder,ps+((it-is)+1),pe,it+1,ie);
+        root->left = helper(preorder,ps+1,ps+noleft,inorder,is,is+noleft);
+        root->right= helper(preorder,ps+noleft+1,pe,inorder,is+noleft+1,ie);
         
         return root;
     }
-    
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        // preorder root left right
-        // inorder left root right 
-       
-        int ps = 0;
-        int is = 0;
-        int pe = preorder.size()-1;
-        int ie = inorder.size()-1;
-        return helper(preorder,inorder, ps,pe, is, ie);
-        
-        
-        
-        
-        
-        
-        
-        ////// 06/01/2022
-        
-//         if(preorder.size()==0 || inorder.size()==0) return NULL;
-        
-//         TreeNode* root = new TreeNode(preorder[0]);
-//         int mid = find(inorder.begin(),inorder.end(),root->val)-inorder.begin();
-        
-//         // cut preorder in => left= mid ele from starting , right = inordersize-mid-1 elements from starting+left-ele 
-//         auto is = inorder.begin();
-//         auto ie = inorder.end();
-//         auto ps = preorder.begin();
-//         auto pe = preorder.end();
-//         vector<int>nil(is,is+mid);
-//         vector<int>nir(is+mid+1,ie);
-        
-//         vector<int>npl(ps+1,ps+1+nil.size());
-//         vector<int>npr(ps+1+npl.size(),pe);
-//         // for(auto e:nil)cout<<e<<" ";
-//         // cout<<endl;
-//         // for(auto e:nir)cout<<e<<" ";
-//         // cout<<endl;
-//         // for(auto e:npl)cout<<e<<" ";
-//         // cout<<endl;
-//         // for(auto e:npr)cout<<e<<" ";
-//         // cout<<endl;
-        
-//         root->left=buildTree(npl,nil);
-//         root->right=buildTree(npr,nir);
-        
-//         return root;
-        
-        
-        
-        // return helper(preorder,inorder,0,inorder.size()-1);
-    }
-    
-    /*
-    int cnt=0;
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int st,int en){
-        if(st<0 ||en>=inorder.size())return NULL;
-        if(st>en)return NULL;
-        TreeNode* head=new TreeNode(preorder[cnt]);
-        cnt++;
-        
-        
-        int t=st;
-        while(t<=en){
-            if(head->val==inorder[t]) break;
-            t++;
-        }
-        
-        head->left=helper(preorder,inorder,st,t-1);
-        head->right=helper(preorder,inorder,t+1,en);
-        
-        return head;
-    }
-    */
 };
