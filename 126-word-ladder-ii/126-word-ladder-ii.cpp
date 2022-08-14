a5d2343326ed66013970ae7faecc3fdb42fcb3b1
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-14 13:41:19 -0700

    Time: 29 ms (53.66%), Space: 9.6 MB (70.31%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,59 +1,77 @@
 class Solution {
-    void dfs(unordered_map<string, vector<string>>& adjList,vector<vector<string>>& ladders, vector<string>& seq, string word, string beginWord){
-        if (word == beginWord){
-            ladders.push_back(seq);
+public:
+    
+    unordered_map<string,vector<string>>parent;
+    unordered_map<string,int>dist;
+    vector<vector<string>>ans;
+    
+    void dfs(string curr,vector<string>&path){
+        if(curr == "root"){
+            ans.push_back(path);
             return;
         }
-        vector<string> wordList = adjList[word];
-        for(int i=0; i<wordList.size(); i++){
-            seq.push_back(wordList[i]);
-            dfs(adjList,ladders, seq, wordList[i], beginWord);
-            seq.pop_back();
+
+        for(auto par:parent[curr]){        
+            path.push_back(curr);
+            dfs(par,path);
+            path.pop_back();
         }
-        return;
     }
-public:
+    
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        // Get unique words and add beginWord if not exists
-        unordered_set<string> uniqueWords;
-        uniqueWords.insert(beginWord);
-        for(string word: wordList){
-            uniqueWords.insert(word);
+         // use dyjstra to get the min dist to the endword 
+        // and fill parent array 
+        
+        for(auto s:wordList){
+            dist[s] = INT_MAX;
+            parent[s] = {};
         }
-        unordered_set<string> visited;
-        vector<vector<string>> ladders;
-        queue<string> q;
+        
+        dist[beginWord] = 1;
+        parent[beginWord] = {"root"};
+        
+        queue<string>q;
         q.push(beginWord);
-        visited.insert(beginWord);
-        unordered_map<string, vector<string>> adjList;
+        
         while(!q.empty()){
-            int nodeCount = q.size();
-            while(nodeCount--){
-                string lastWordInSeq = q.front();
-                q.pop();
-                for(int i=0; i<lastWordInSeq.size(); i++){
-                    string currWord = lastWordInSeq;
-                    for(char c = 'a'; c<='z'; c++){
-                        if (c == currWord[i]){
-                            continue;
-                        }
-                        currWord[i] = c;
-                        if (uniqueWords.find(currWord) != uniqueWords.end()){
-                            adjList[currWord].push_back(lastWordInSeq);
-                            if (visited.find(currWord) == visited.end()){
-                                q.push(currWord);
-                            }
-                            visited.insert(currWord);
-                        }
-                    }
-                }
+            
+            string currWord = q.front();
+            q.pop();
+            
+            if(currWord == endWord){
+                break;
             }
-            for(string word: visited) uniqueWords.erase(word);
-            visited.clear();
+            
+            for(int i=0;i<currWord.size();i++){
+                string newWord = currWord;
+                for(auto ch='a';ch<='z';ch++){
+                    
+                    newWord[i] = ch;
+                    
+                    if(newWord != currWord && dist.count(newWord)){
+                        if(dist[newWord] > dist[currWord] + 1){
+                            dist[newWord] = 1 + dist[currWord];
+                            parent[newWord].clear();
+                            parent[newWord].push_back(currWord);
+                            q.push(newWord);
+                        }
+                        else if(dist[newWord] == dist[currWord] + 1){   // if the dist to newword is same as currdist 
+                            parent[newWord].push_back(currWord);        // then we found another path to it now
+                        }
+                    } 
+                }   
+            }
+             
+        }   
+        
+        vector<string>path;
+        
+        dfs(endWord,path);
+        
+        for(auto &vec:ans){
+            reverse(vec.begin(),vec.end());
         }
-        vector<string> currSeq(1, endWord);
-        dfs(adjList, ladders, currSeq, endWord, beginWord);
-        for(int i=0; i<ladders.size(); i++) reverse(ladders[i].begin(), ladders[i].end());
-        return ladders;   
+
+        return ans;
     }
 };
