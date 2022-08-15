e81f71fce6b63d2ba0b8354efaf48cdb6c502d05
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-15 04:28:47 -0700

    Time: 4 ms (91.82%), Space: 8.4 MB (94.58%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -23,22 +23,28 @@ class Solution {
 public:
     Node* cloneGraph(Node* node) {
         if(node==NULL) return NULL;
-        
         unordered_map<Node*,Node*>m;
-        m[node]=new Node(node->val,{});
-        // bfs 
+        
+        // starting node 
+        m[node]=new Node(node->val);
+        
+        // using bfs
         queue<Node*>q;
         q.push(node);
         
         while(q.size()){
-            Node* t = q.front(); q.pop();
-            if(m.find(t)!=m.end() && m[t]->neighbors.size()!=0)continue;
+            Node* t = q.front();
+            q.pop();
+            
+            // if curr node's already has some neighbours we dont need to use it 
+            if(m.find(t)!=m.end() && m[t]->neighbors.size()!=0) continue;
             
             for(auto nei : t->neighbors){
                 if(m.find(nei)==m.end()){
-                    m[nei]=new Node(nei->val,{});
-                    q.push(nei);
+                    m[nei]=new Node(nei->val);
+                    q.push(nei); // push because we need to clone its neib too 
                 }
+                // pushing copy of nei to copy of t 
                 m[t]->neighbors.push_back(m[nei]);
             }
         }
