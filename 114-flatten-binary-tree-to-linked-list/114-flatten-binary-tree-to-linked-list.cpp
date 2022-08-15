b650a42bde7f65be6ebaff6ddb89cdc396e5557a
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-14 23:27:41 -0700

    Time: 0 ms (100.00%), Space: 12.7 MB (38.63%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -12,37 +12,20 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        // iterative morris traversal O(3n) , O(1)
-
+        // morris 
+        
         TreeNode* curr = root;
-        while(curr!=NULL){
+        while(curr)
+        {
             if(curr->left){
-                TreeNode* pred = curr->left;
-                while(pred->right){
-                    pred = pred->right;
-                }
-                pred->right = curr->right;
+                TreeNode* prev = curr->left;
+                while(prev->right) prev = prev->right;
+                prev->right = curr->right;
                 curr->right = curr->left;
-                curr->left = NULL;
-            }else{
-                curr = curr->right;
+                curr->left=NULL;
             }
+            curr = curr->right;
         }
         
-        // recursive O(n) , O(n)
-        // if(!root) return ;
-        // vector<TreeNode*>v;
-        // preo(root,v);
-        // for(int i=0;i<v.size()-1;i++){
-        //     v[i]->left=NULL;
-        //     v[i]->right=v[i+1];
-        // }
-    }
-    
-    void preo(TreeNode* root, vector<TreeNode*>&v){
-        if(!root) return ;
-        v.push_back(root);
-        preo(root->left,v);
-        preo(root->right,v);
     }
 };
