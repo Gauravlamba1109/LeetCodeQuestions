fc9804e6aa21d64f60098a2d4b5d353377023fc2
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-08-16 11:20:48 -0700

    Time: 1354 ms (27.35%), Space: 339.6 MB (17.21%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int len;
+    bool match(string t, unordered_map<string,int>w){
+        for(int j=0;j<t.size(); j+=len){
+            string temp = t.substr(j,len);
+            if(w.find(temp)!=w.end()){
+                if(--w[temp]==-1) return false;
+            }
+            else return false;
+        }
+        return true;
+        
+//         vector<bool>dp(t.size()+1,0);
+//         dp[t.size()]=1;
+//         int s=(*w.begin()).first.size();
+        
+//         for(int i=t.size()-1;i>=0;i--){
+//             string temp=t.substr(i,s);
+//             if(w.find(temp)!=w.end() && w[temp]>0 &&(i+s)<=t.size() && dp[i+s]==1){
+//                 dp[i]=1;
+//                 w[temp]--;
+//             }
+//         }
+//         return dp[0];
+    }
+    
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int window = words.size()*words[0].size();
+        len = words[0].size();
+        int i=0;
+        vector<int>ans;
+        unordered_map<string,int>mp;
+        
+        for(auto w:words){
+            mp[w]+=1;
+        }
+        
+        while(i<(s.size()-window+1)){
+            if(match(s.substr(i,window),mp)){
+                ans.push_back(i);
+            }
+            i++;
+        }
+        
+        return ans;
+    }
+};
