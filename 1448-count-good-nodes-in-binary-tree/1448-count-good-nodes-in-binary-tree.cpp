86b275804c2ad83114dfa4014c2d9cdacbe480eb
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-09-02 01:34:24 -0700

    Time: 212 ms (47.24%), Space: 90.2 MB (5.06%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,23 +11,49 @@
  */
 class Solution {
 public:
-    int cnt=0;
-    int goodNodes(TreeNode* root) {
-        // dfs to leaf with a max value 
-        if(!root) return 0;
-        int maxi = INT_MIN;
-        helper(root,maxi);
-        return cnt ;
-    }
-    
-    void helper(TreeNode* root, int maxi){
-        if(root==NULL) return ;
-        else{
-            if(root->val>=maxi)cnt++;
-            maxi = max(maxi,root->val);
-            helper(root->left,maxi);
-            helper(root->right,maxi);
+   int goodNodes(TreeNode* root) {
+        // using bfs with a max value attached 
+       int m = INT_MIN;
+       int cnt=0;
+       queue<pair<TreeNode*,int>>q;
+       if(!root) return 0;
+       q.push({root,m});
+       
+       while(q.size()){
+           auto t = q.front(); q.pop();
+           TreeNode* curr = t.first;
+           int curr_maxi = t.second;
+           if(curr_maxi<=curr->val)cnt++;
+           
+           if(curr->left){
+               q.push({curr->left,max(curr_maxi,curr->val)});
+           }
+           if(curr->right){
+               q.push({curr->right,max(curr_maxi,curr->val)});
+           }
         }
-        return ;
-    }
+        
+       return cnt;
+   }
+    
+    
+//     int cnt=0;
+//     int goodNodes(TreeNode* root) {
+//         // dfs to leaf with a max value 
+//         if(!root) return 0;
+//         int maxi = INT_MIN;
+//         helper(root,maxi);
+//         return cnt ;
+//     }
+    
+//     void helper(TreeNode* root, int maxi){
+//         if(root==NULL) return ;
+//         else{
+//             if(root->val>=maxi)cnt++;
+//             maxi = max(maxi,root->val);
+//             helper(root->left,maxi);
+//             helper(root->right,maxi);
+//         }
+//         return ;
+//     }
 };
