708e46fc585d5c080c627caa937d3aa9845a4cb9
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-09-06 03:08:37 -0700

    Time: 423 ms (69.22%), Space: 8 MB (44.78%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0;i<board.size();i++){
+        // backtracking 
+        
+        for(int i = 0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]==word[0]){
-                    bool f = dfs(board,word,i,j,0);
-                    if(f) return true;
+                    set<pair<int,int>>p;
+                    bool t = dfs(i,j,board,word,0,p);
+                    if(t) return true;
                 }
             }
         }
         return false;
     }
     
-    vector<vector<int>>dir = {{0,1},{1,0},{-1,0},{0,-1}};
-    bool dfs(vector<vector<char>>& board, string word,int i,int j,int k){
-        if(k==word.size()) return true;
-        if(i>=board.size() || j>=board[0].size() || i<0 || j<0 || board[i][j]!=word[k]) return false;
-        
-        char temp = board[i][j];
-        board[i][j]='.';
-        
-        bool t1 = dfs(board,word,i+1,j,k+1); if(t1) return true;
-        bool t2 = dfs(board,word,i-1,j,k+1); if(t2) return true;
-        bool t3 = dfs(board,word,i,j+1,k+1); if(t3) return true;
-        bool t4 = dfs(board,word,i,j-1,k+1); if(t4) return true;
+    bool dfs(int i,int j , vector<vector<char>>& board, string &word, int idx, set<pair<int,int>>&path){
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[idx]){
+            return false;
+        }
 
-        board[i][j]=temp;
+        if(idx==word.size()-1)return true;
         
-        return t1||t2||t3||t4;
+        board[i][j]='#';
+        
+        if (dfs(i+1,j,board,word,idx+1,path)
+            ||dfs(i-1,j,board,word,idx+1,path)
+            ||dfs(i,j+1,board,word,idx+1,path)
+            ||dfs(i,j-1,board,word,idx+1,path))return true;
+        
+        board[i][j]=word[idx];
+        return false;
+        
     }
 };
-
-
-
-
-
-
-
-
