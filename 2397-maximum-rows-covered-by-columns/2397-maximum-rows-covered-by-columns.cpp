dde688318b4ad8978216e4ee92477919d21db2b6
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-09-13 10:36:21 -0700

    Time: 10 ms (75.84%), Space: 8.6 MB (66.94%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -1,46 +1,53 @@
-// select numSelect columns from mat and count the max covered rows 
-// then backtarck and select different combin and repeat
-
-class Solution {
+class Solution 
+{
+    int m;
+    int n;
+    vector<bool> selected;
+    int ans = 0;
 public:
-    int ans;
-    int maximumRows(vector<vector<int>>& mat, int numSelect) {
-        ans=0;
-        int n = mat.size(), m = mat[0].size() ;
-        vector<int>chos(m,0);
-        helper(mat,chos,0,n,m,numSelect);
+    int maximumRows(vector<vector<int>>& matrix, int numSelect) 
+    {
+        m = matrix.size();
+        n = matrix[0].size();
+        
+        selected.resize(n);
+        dfs(matrix, numSelect, 0);
+        
         return ans;
     }
     
-    // O(2^m) * O(n*m) = nm*(2^m)
-    void helper(vector<vector<int>>& mat,vector<int>&chose, int c, int n ,int m, int cols){
-        if(cols == 0){
-            int cntRows = calcRow(mat,chose,n,m);
-            ans = max(ans,cntRows);
-            return ;
-        }
-        
-        if(c>=m) return ;
-        
-        helper(mat,chose,c+1,n,m,cols);
-        chose[c]=1;
-        helper(mat,chose,c+1,n,m,cols-1);
-        chose[c]=0;
-    }
-    
-    // covered count o(n*m)
-    int calcRow(vector<vector<int>>& mat,vector<int>&chose, int n ,int m){
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            bool ok = 1;
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1 && chose[j]==0){ // if its set and still not choosen 
-                    ok = 0;
-                    break;
+    void dfs(vector<vector<int>>& mat, int cnt, int cur)
+    {
+        if (cnt == 0)
+        {
+            int rows = 0;
+            for (int i = 0; i < m; ++i)
+            {
+                bool covered = true;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (mat[i][j] == 1 && !selected[j])
+                    {
+                        covered = false;
+                        break;
+                    }
+                }
+                
+                if (covered)
+                {
+                    ++rows;
                 }
             }
-            if(ok)cnt++;
+            
+            ans = max(ans, rows);
+            return;
         }
-        return cnt;
+        
+        for (int k = cur; k < n; ++k)
+        {
+            selected[k] = true;
+            dfs(mat, cnt - 1, k + 1);
+            selected[k] = false;
+        }
     }
 };
