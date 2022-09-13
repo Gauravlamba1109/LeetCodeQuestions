f5fea57b85d36816f8be85741836caf3bddf0d70
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-09-13 10:28:12 -0700

    Time: 4 ms (92.82%), Space: 8.6 MB (66.94%) - LeetHub

---
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int ans;
+    int maximumRows(vector<vector<int>>& mat, int numSelect) {
+        ans=0;
+        int n = mat.size(), m = mat[0].size() ;
+        vector<int>chos(m,0);
+        helper(mat,chos,0,n,m,numSelect);
+        return ans;
+    }
+    
+    void helper(vector<vector<int>>& mat,vector<int>&chose, int c, int n ,int m, int cols){
+        if(cols == 0){
+            int cntRows = calcRow(mat,chose,n,m);
+            ans = max(ans,cntRows);
+            return ;
+        }
+        
+        if(c>=m) return ;
+        
+        helper(mat,chose,c+1,n,m,cols);
+        chose[c]=1;
+        helper(mat,chose,c+1,n,m,cols-1);
+        chose[c]=0;
+    }
+    
+    // covered count 
+    int calcRow(vector<vector<int>>& mat,vector<int>&chose, int n ,int m){
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            bool ok = 1;
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1 && chose[j]==0){ // if its set and still not choosen 
+                    ok = 0;
+                    break;
+                }
+            }
+            if(ok)cnt++;
+        }
+        
+        return cnt;
+    }
+};
