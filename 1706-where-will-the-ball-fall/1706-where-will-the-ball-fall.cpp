fe4b1472ec72c1b1511bd962ce880654f23c706f
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2022-11-01 13:08:51 +0530

    Time: 22 ms (97.55%), Space: 13.1 MB (80.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
         vector<int>ans(grid[0].size(),0);
+        // doin a dfs form each jth pos 
+        // if its not valid it will put -1 in ans;
+        
         for(int i=0;i<grid[0].size();i++){
             ans[i]=dfs(grid,0,i);
         }
@@ -11,9 +14,14 @@ public:
     
     
     int dfs(vector<vector<int>>&grid,int i,int j){
+        
+        // if reached end then put j ;
         if(i==grid.size()) return j;
         
+        // where will it go from here
         int nxtCol = j + grid[i][j] ;
+        
+        // if the nxtcol is not valid then we cant move there 
         if(nxtCol<0 || nxtCol>grid[0].size()-1 || grid[i][j]!=grid[i][nxtCol]) return -1;
         
         return dfs(grid,i+1,nxtCol);
