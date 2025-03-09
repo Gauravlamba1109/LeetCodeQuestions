69dfc51566326e9f5d952a97b6c09412f2c619ad
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-03-10 02:32:52 +0530

    Time: 72 ms (55.06%), Space: 102.6 MB (77%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        // sliding window 
-        // l is the starting point , compare starts with r till k eles are reached
-        // after k ele reset l 
-
-        int l=0;
-        int r=1;
-        int ans = 0;
-        int n = colors.size() ;
-        for(r=1; r< n+(k-1) ; r++){
-            if(colors[r%n]==colors[(r-1)%n]){
-                l = r; // adj ele are equal ; pattern break ; reset window ; 
+        int cnt = 1 ; 
+        int prev = colors[0];
+        int n = colors.size();
+        int ans = 0 ;
+        for(int i=0;i<n+k-1;i++){
+            if(colors[i%n]==prev) {
+                cnt = 1;
+            }else{
+                cnt ++ ;
+                if( cnt >= k) ans++; // found one more alternating window
             }
-            if( r - l + 1 > k ){    // pattern confinues but window too big, shift left pointer 
-                l ++ ; 
-            }
-            if( r - l + 1 == k ){
-                ans ++ ; 
-            }
+            prev= colors[i%n];
         }
-
         return ans;
     }
 };
