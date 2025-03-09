bd77a2461922e165fdc1926ef29bda14149c5cac
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-03-10 05:23:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (59.82%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        // mini W in k eles 
-        int prev=blocks[0];
-
         int ans=10000;
-        int n= blocks.size();
-  
+        int n= blocks.size();  
         int cnt=0;
-        for(int i=0;i<k;i++){
+        for(int i=0;i<k;i++)
             if(blocks[i]=='W') cnt++;
-        } //BWBBWWBBBWB
+        
         ans=cnt;
-        int l=0; int r=0;
-        cout<<n<<endl;
-        for(r=k;r<n;r++){
-            cout<<blocks[r%n]<<": "<<r<<"=r; r-k=" <<blocks[(r-k)%n] <<" : cnt"<<cnt<<endl;
-            string a = blocks.substr((r-k)%n, k); 
-            cout<<a;
-            cout<<blocks.substr(0,k-a.size())<<endl;
-            if(blocks[r%n]=='W'){
-                cnt++;
-            }
-            if(blocks[(r-k)%n]=='W'){
-                cnt--;
-            }
+        for(int r=k;r<n;r++){
+            if(blocks[r]=='W') cnt++;
+            if(blocks[(r-k)]=='W') cnt--;
             ans=min(ans,cnt);
         }
         return ans;
