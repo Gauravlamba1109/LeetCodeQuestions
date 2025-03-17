ca032c5d5dc28651aa9eb22746e073109296a9d0
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-03-17 23:14:22 +0530

    Time: 6 ms (24.13%), Space: 18.2 MB (31.79%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -4,9 +4,10 @@ public:
         unordered_map<int,int>m;
         for(auto e : nums){
             m[e]++;
+            m[e]%=2;
         }
         for(auto e : m){
-            if(e.second%2 != 0) return false;
+            if(e.second != 0) return false;
         }
 
         return true;
