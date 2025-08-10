5c7d990376f3a3b90fb5dd7f7c17c183ada5e602
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-08-10 05:51:44 +0530

    Time: 166 ms (51.73%), Space: 11.1 MB (95.93%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
+    int dp[301][301];
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0));
-        return dfs(nums, 1, nums.size()-2,dp);
+        memset(dp,-1,sizeof(dp));
+
+        return dfs(nums, 1, nums.size()-2);
     }
 
-    int dfs( vector<int> &nums, int l , int r,vector<vector<int>>&dp){
+    int dfs( vector<int> &nums, int l , int r){
         if(l>r) return 0;
 
-        if(dp[l][r]!=0) return dp[l][r];
+        if(dp[l][r]!=-1) return dp[l][r];
 
         for(int i=l; i<=r; i++){
             // if curr is popped last 
             int cost = nums[l-1]*nums[i]*nums[r+1];
-            cost =  cost + (dfs(nums, l, i-1,dp) + dfs(nums, i+1, r,dp));
+            cost =  cost + (dfs(nums, l, i-1) + dfs(nums, i+1, r));
             dp[l][r] = max(dp[l][r],cost);
         }
         return dp[l][r];
