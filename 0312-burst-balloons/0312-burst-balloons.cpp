836de59b4be5c0dbf9be0d1821af1dc4739c52bb
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-08-10 05:44:14 +0530

    Time: 251 ms (5.01%), Space: 14.1 MB (6.28%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -10,16 +10,14 @@ public:
     int dfs( vector<int> &nums, int l , int r,vector<vector<int>>&dp){
         if(l>r) return 0;
 
-        int ans= INT_MIN;
         if(dp[l][r]!=0) return dp[l][r];
 
         for(int i=l; i<=r; i++){
             // if curr is popped last 
             int cost = nums[l-1]*nums[i]*nums[r+1];
             cost =  cost + (dfs(nums, l, i-1,dp) + dfs(nums, i+1, r,dp));
-            ans = max(ans,cost);
+            dp[l][r] = max(dp[l][r],cost);
         }
-        dp[l][r]=ans;
-        return ans;
+        return dp[l][r];
     }
 };
