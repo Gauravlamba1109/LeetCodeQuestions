c5e140228e3d8f6414e32181af0c15affa28edd4
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2025-08-18 10:06:51 +0530

    Time: 11 ms (12.28%), Space: 12.7 MB (20.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<string,int>>sym = {
-            {"I",1},{"IV",4},{"V",5},{"IX",9},
-            {"X",10},{"XL",40},{"L",50},{"XC",90},
-            {"C",100},{"CD",400},{"D",500},{"CM",900},
-            {"M",1000}
-        };
-        string ans="";
+        vector<pair<string, int>> sym = {
+            {"I", 1},   {"IV", 4},   {"V", 5},   {"IX", 9},  {"X", 10},
+            {"XL", 40}, {"L", 50},   {"XC", 90}, {"C", 100}, {"CD", 400},
+            {"D", 500}, {"CM", 900}, {"M", 1000}};
+        string ans = "";
 
         int n = sym.size();
-        for(int i=n-1; i>=0; i--){
-            int rem = num%sym[i].second;
-            int d = (num-rem)/sym[i].second;
-
-            while(d--){
-                ans+=sym[i].first;
-            }
-            num=rem;
+        for (int i = n - 1; i >= 0; i--) {
+            int rem = num % sym[i].second;
+            int d = (num - rem) / sym[i].second;
+            while (d--)
+                ans.append(sym[i].first);
+            num = rem;
         }
 
         return ans;
