9bc51fe1a8af4798d745a03ca187b5538d7a23d6
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2026-02-22 22:41:46 +0530

    Time: 0 ms (100%), Space: 41 MB (42.23%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
@@ -3,24 +3,23 @@ public:
     int scoreDifference(vector<int>& nums) {
         // simulation
         int fscore=0;
-        int sscore=0;
         bool fturn = 1;
         for(int i=0;i<nums.size();i++){
+            
             if((nums[i]%2==1)){ 
                 fturn = (fturn==1)?0:1;
             }
-
-            if((i+1)%6==0){
+            if(i%6==5){
                 fturn = (fturn==1)?0:1;
             }
 
             if(fturn){
                 fscore+=nums[i];
             }else{
-                sscore+=nums[i];
+                fscore-=nums[i];
             }
         }
 
-        return fscore-sscore;
+        return fscore;
     }
 };
