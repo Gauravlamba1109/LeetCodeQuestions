f6cd362e37f38495364331a71e61f14744fb476e
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2026-07-15 01:37:32 +0530

    Time: 11 ms (26.44%), Space: 12.1 MB (70.62%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,60 +1,74 @@
 class Solution {
 public:
+    unordered_map<char,int> m = {
+{'I'     ,        1},
+{'V'      ,       5},
+{'X'       ,      10},
+{'L'        ,     50},
+{'C'         ,    100},
+{'D'          ,   500},
+{'M'           ,  1000}
+    };
+
     int romanToInt(string s) {
 
         int i=0;
         long num=0;
-        while(i<s.size()){
-            if(s[i]=='M') num+=1000;
-            else if(s[i]=='D') num+=500;
-            else if(s[i]=='C'){
-                if(i+1<s.size()){
-                    if(s[i+1]=='D'){
-                        num+=400;
-                        i++;
-                    }
-                    else if(s[i+1]=='M'){
-                        num+=900;
-                        i++;
-                    }
-                    else num+=100;
-                }
-                else num+=100;
-            } 
-            else if(s[i]=='L') num+=50;
-            else if(s[i]=='X'){
-                if(i+1<s.size()){
-                    if(s[i+1]=='L'){
-                        num+=40;
-                        i++;
-                    }
-                    else if(s[i+1]=='C'){
-                        num+=90;
-                        i++;
-                    }
-                    else num+=10;
-                }
-                else num+=10;
-            }
-            else if(s[i]=='V') num+=5; 
-            else if(s[i]=='I'){
-                if(i+1<s.size()){
-                    if(s[i+1]=='V'){
-                        num+=4;
-                        i++;
-                    }
-                    else if(s[i+1]=='X'){
-                        num+=9;
-                        i++;
-                    }
-                    else num+=1;
-                }
-                else num+=1;
-            }
-            // cout<<"i:"<<i<<" num="<<num<<endl;
-            i++;
-        }
+        // while(i<s.size()){
+        //     if(s[i]=='M') num+=1000;
+        //     else if(s[i]=='D') num+=500;
+        //     else if(s[i]=='C'){
+        //         if(i+1<s.size()){
+        //             if(s[i+1]=='D'){
+        //                 num+=400;
+        //                 i++;
+        //             }
+        //             else if(s[i+1]=='M'){
+        //                 num+=900;
+        //                 i++;
+        //             }
+        //             else num+=100;
+        //         }
+        //         else num+=100;
+        //     } 
+        //     else if(s[i]=='L') num+=50;
+        //     else if(s[i]=='X'){
+        //         if(i+1<s.size()){
+        //             if(s[i+1]=='L'){
+        //                 num+=40;
+        //                 i++;
+        //             }
+        //             else if(s[i+1]=='C'){
+        //                 num+=90;
+        //                 i++;
+        //             }
+        //             else num+=10;
+        //         }
+        //         else num+=10;
+        //     }
+        //     else if(s[i]=='V') num+=5; 
+        //     else if(s[i]=='I'){
+        //         if(i+1<s.size()){
+        //             if(s[i+1]=='V'){
+        //                 num+=4;
+        //                 i++;
+        //             }
+        //             else if(s[i+1]=='X'){
+        //                 num+=9;
+        //                 i++;
+        //             }
+        //             else num+=1;
+        //         }
+        //         else num+=1;
+        //     }
+        //     // cout<<"i:"<<i<<" num="<<num<<endl;
+        //     i++;
+        // }
         
+        for(int i=0;i<s.size();i++){
+            if( i+1<s.size() && m[s[i]] < m[s[i+1]]) num-=m[s[i]];
+            else num+=m[s[i]];
+        }
         return num;
     }
 };
