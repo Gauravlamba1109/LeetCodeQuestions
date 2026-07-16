8253fcf81ea76ced7d2cdc2550c4fab306905830
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2026-07-16 23:15:11 +0530

    Time: 2 ms (88.53%), Space: 169.3 MB (64.86%) - LeetHub

--- 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
+++ 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
@@ -3,33 +3,33 @@ public:
     int minimumCost(vector<int>& nums, int k) {
         long mod = 1000000007;
         long cost = 0;
-        int times= 0;
-        int res = k;
-        for(int i=0;i<nums.size();i++){
-            // cout<<nums[i]<<" k="<<k<<" "<<endl;
-            if(nums[i]>k){
-                int extra = nums[i]-k;
-                int t = extra/res;
-                int m = extra%res;
-                if(t==0 || m>0 ) {t++;}
-                k+=(res*t);
-                times=(times+t)%mod;
-                // cout<<"t="<<t<<endl;
-            }
-            k-=nums[i];
-            if(k<0 || (k==0 && (i+1)<nums.size())){
-                k+=res;
-                times++;
-            }
-        }
+        // int times= 0;
+        // int res = k;
+        // for(int i=0;i<nums.size();i++){
+        //     // cout<<nums[i]<<" k="<<k<<" "<<endl;
+        //     if(nums[i]>k){
+        //         int extra = nums[i]-k;
+        //         int t = extra/res;
+        //         int m = extra%res;
+        //         if(t==0 || m>0 ) t++;
+        //         k+=(res*t);
+        //         times=(times+t)%mod;
+        //         // cout<<"t="<<t<<endl;
+        //     }
+        //     k-=nums[i];
+        //     if(k<0 || (k==0 && (i+1)<nums.size())){
+        //         k+=res;
+        //         times++;
+        //     }
+        // }
 
-        // cost = (1)+2
+        long long t = 0;
+        for(auto e: nums) {t+=e;}
+        long long times = (t+k-1)/k % mod ;
+        // here we exclude the last k addition, mean only times-1 times we have to increase 
 
         cout<<times<<endl;
-        cost=((long)times*(times+1)/2)%mod;
-
-        // cout<<"times*times+1="<<((long)times*(times+1)/2)%mod<<endl;
+        cost=((long)times*(times-1)/2)%mod;
         return cost;
     }
 };
-
